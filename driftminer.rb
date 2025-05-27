10cb8a1f48dc90c2e4eb356e9d3a2ea552cd4ce9
Scott Howard <scotthoward@mac.lan>
2025-05-27 19:38:38 -0400

    Update RB

--- driftminer.rb
+++ driftminer.rb
@@ -1,28 +1,19 @@
 class Driftminer < Formula
-  desc "Drift Detection as Code - Define custom infrastructure drift detection rules"
+  desc "Drift Detection as Code"
   homepage "https://github.com/ScottRyanHoward/driftminer"
   url "https://github.com/ScottRyanHoward/driftminer/archive/refs/tags/v0.1.2.tar.gz"
   sha256 "d55da981c65acc102eb9f3de4a675ff07a0216305e5d50bd3504059b58a63236"
   license "MIT"
+  head "https://github.com/ScottRyanHoward/driftminer.git", branch: "main"
 
-  depends_on "python@3.12"
+  depends_on "go" => :build
 
   def install
-    python = Formula["python@3.12"].opt_bin/"python3"
-    venv = libexec
-
-    # Create venv
-    system python, "-m", "venv", "#{venv}"
-    venv_python = venv/"bin/python"
-
-    # Install driftminer
-    system venv_python, "-m", "pip", "install", "."
-
-    # Create symlinks
-    bin.install_symlink Dir["#{venv}/bin/*"]
+    system "go", "build", *std_go_args(ldflags: "-s -w"), "./main.go"
   end
 
   test do
-    system bin/"driftminer", "--version"
+    system "#{bin}/driftminer", "--help"
   end
 end
+
