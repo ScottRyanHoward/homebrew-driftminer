89cc415c56fbf82c4c316cf5fef4d672e165b447
Scott Howard <scotthoward@mac.lan>
2025-05-27 19:55:24 -0400

    Update RB

--- driftminer.rb
+++ driftminer.rb
@@ -2,7 +2,7 @@ class Driftminer < Formula
   desc "Drift Detection as Code"
   homepage "https://github.com/ScottRyanHoward/driftminer"
   url "https://github.com/ScottRyanHoward/driftminer/releases/download/v0.1.2/driftminer-0.1.2.tar.gz"
-  sha256 "0019dfc4b32d63c1392aa264aed2253c1e0c2fb09216f8e2cc269bbfb8bb49b5"
+  sha256 "d55da981c65acc102eb9f3de4a675ff07a0216305e5d50bd3504059b58a63236"
   version "0.1.2"
 
   def install
