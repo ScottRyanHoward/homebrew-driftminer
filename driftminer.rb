9b8b77a15b1bb717b5a16a884feb9bfd0a8d3bd1
Scott Howard <scotthoward@mac.lan>
2025-05-27 19:14:53 -0400

    Update Python install

--- driftminer.rb
+++ driftminer.rb
@@ -1,4 +1,5 @@
 class Driftminer < Formula
+  include Language::Python::Virtualenv
   desc "Drift Detection as Code - Define custom infrastructure drift detection rules"
   homepage "https://github.com/ScottRyanHoward/driftminer"
   url "https://github.com/ScottRyanHoward/driftminer/archive/refs/tags/v0.1.2.tar.gz"
@@ -8,7 +9,10 @@ class Driftminer < Formula
   depends_on "python@3.12"
 
   def install
-    system Formula["python@3.12"].opt_bin/"python3", "-m", "pip", "install", "--prefix=#{prefix}", "."
+    python3 = Formula["python@3.12"].opt_bin/"python3"
+    site_packages = Language::Python.site_packages(python3)
+    ENV.prepend_path "PYTHONPATH", prefix/site_packages
+    system python3, "-m", "pip", "install", "--target=#{prefix/site_packages}", "."
   end
 
   test do
