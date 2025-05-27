4b25c6fd6e1455d7516588ebf88a9586e4669c3b
Scott Howard <scotthoward@mac.lan>
2025-05-27 19:29:36 -0400

    fix: Use virtualenv for installation

--- driftminer.rb
+++ driftminer.rb
@@ -7,18 +7,30 @@ class Driftminer < Formula
 
   depends_on "python@3.12"
 
+  resource "virtualenv" do
+    url "https://files.pythonhosted.org/packages/d6/45/cf2c3c08348a6bd6a2b7f3c1347f0d40f36df0f2f7980dd9c3a27b9dc4c6/virtualenv-20.25.0.tar.gz"
+    sha256 "bf51c0d9c7dd63ea8e44086fa1e4fb1093a31e963b86959257378aef020e1f1b"
+  end
+
   def install
     python = Formula["python@3.12"].opt_bin/"python3"
-    ENV["PYTHONPATH"] = lib/"python3.12/site-packages"
+    venv = libexec
 
-    resources.each do |r|
-      r.stage do
-        system python, "-m", "pip", "install", ".", "--target=#{lib}/python3.12/site-packages"
-      end
+    # Install virtualenv
+    resource("virtualenv").stage do
+      system python, "-m", "pip", "install", ".", "--target=#{libexec}"
     end
 
-    system python, "-m", "pip", "install", ".", "--target=#{lib}/python3.12/site-packages"
-    bin.install_symlink Dir["#{lib}/python3.12/site-packages/bin/*"]
+    # Create virtualenv
+    ENV.prepend_path "PYTHONPATH", libexec
+    system python, "-m", "virtualenv", "#{venv}"
+    venv_python = venv/"bin/python"
+
+    # Install driftminer in virtualenv
+    system venv_python, "-m", "pip", "install", "."
+
+    # Create symlinks
+    bin.install_symlink Dir["#{venv}/bin/*"]
   end
 
   test do
