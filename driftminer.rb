e1a1bf3a89ffde56991f91bc063e156409e8bb18
Scott Howard <scotthoward@mac.lan>
2025-05-27 18:16:05 -0400

    Add pip

--- driftminer.rb
+++ driftminer.rb
@@ -1,6 +1,4 @@
 class Driftminer < Formula
-  include Language::Python::Virtualenv
-
   desc "Drift Detection as Code - Define custom infrastructure drift detection rules"
   homepage "https://github.com/ScottRyanHoward/driftminer"
   url "https://github.com/ScottRyanHoward/driftminer/archive/refs/tags/v0.1.1.tar.gz"
@@ -10,10 +8,10 @@ class Driftminer < Formula
   depends_on "python@3.12"
 
   def install
-    # Install pip first
-    system "/opt/homebrew/opt/python@3.12/bin/python3.12", "-m", "ensurepip"
-    system "/opt/homebrew/opt/python@3.12/bin/python3.12", "-m", "pip", "install", "--upgrade", "pip"
-    virtualenv_install_with_resources
+    ENV["PYTHONPATH"] = libexec/"lib/python3.12/site-packages"
+    system "python3.12", "-m", "pip", "install", ".", "--target=#{libexec}/lib/python3.12/site-packages"
+    bin.install Dir[libexec/"bin/*"]
+    bin.env_script_all_files(libexec/"bin", PYTHONPATH: ENV["PYTHONPATH"])
   end
 
   test do
