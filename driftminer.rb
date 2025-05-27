c56b394b154ac9606b5cdd961979003329d24995
Scott Howard <scotthoward@mac.lan>
2025-05-27 18:20:25 -0400

    new version

--- driftminer.rb
+++ driftminer.rb
@@ -1,8 +1,8 @@
 class Driftminer < Formula
   desc "Drift Detection as Code - Define custom infrastructure drift detection rules"
   homepage "https://github.com/ScottRyanHoward/driftminer"
-  url "https://github.com/ScottRyanHoward/driftminer/archive/refs/tags/v0.1.1.tar.gz"
-  sha256 "9d430e63390bc1091f7f1051953a1fa1b53e520a0e61873b8957eb9521f503ef"
+  url "https://github.com/ScottRyanHoward/driftminer/archive/refs/tags/v0.1.2.tar.gz"
+  sha256 "d55da981c65acc102eb9f3de4a675ff07a0216305e5d50bd3504059b58a63236"
   license "MIT"
 
   depends_on "python@3.12"
